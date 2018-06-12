01aa30c37f9031c072b0211c8cdc31dabd46b619
Robley <robleyadrian@gmail.com>
2018-06-12 22:16:45 +0300

    [feat] add error view for 401 Unauthorized.

--- api/web/views/error_view.ex
+++ api/web/views/error_view.ex
@@ -9,6 +9,10 @@ defmodule IntoTheBookmarks.ErrorView do
     %{errors: %{detail: "Internal server error"}}
   end
 
+  def render("401.json", %{message: message}) do
+    %{errors: %{detail: message}}
+  end
+
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
