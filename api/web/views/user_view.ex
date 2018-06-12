cb29bc32e71dccfee68cdbc5ca0a9291b0121a99
Robley <robleyadrian@gmail.com>
2018-06-12 22:17:58 +0300

    [feat] add sign in view to user view.

--- api/web/views/user_view.ex
+++ api/web/views/user_view.ex
@@ -9,6 +9,17 @@ defmodule IntoTheBookmarks.UserView do
     %{user: render_one(user, IntoTheBookmarks.UserView, "user.json")}
   end
 
+  def render("sign_in.json", %{user: user}) do
+    %{
+      data: %{
+        user: %{
+          id: user.id,
+          email: user.email
+        }
+      }
+    }
+  end
+
   def render("user.json", %{user: user}) do
     %{id: user.id,
       email: user.email,
