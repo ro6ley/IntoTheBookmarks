a2ac76ac9828d41a44f664285a4151ab54e4713e
Robley <robleyadrian@gmail.com>
2018-06-12 22:48:26 +0300

    [chore] upgrade phoenix version to 1.3

--- api/mix.exs
+++ api/mix.exs
@@ -31,7 +31,7 @@ defmodule IntoTheBookmarks.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.2.5"},
+      {:phoenix, "~> 1.3.0-rc"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
