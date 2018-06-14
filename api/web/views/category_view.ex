5e7543978734190de7c164ba821c683311e501fd
Robley <robleyadrian@gmail.com>
2018-06-14 08:47:16 +0300

    [fix] use the bookmark view to render list of bookmarks in a category.

--- api/web/views/category_view.ex
+++ api/web/views/category_view.ex
@@ -1,7 +1,10 @@
 defmodule IntoTheBookmarks.CategoryView do
   use IntoTheBookmarks.Web, :view
 
+  alias IntoTheBookmarks.BookmarkView
+
   def render("index.json", %{categories: categories}) do
+    IO.inspect categories
     %{data: render_many(categories, IntoTheBookmarks.CategoryView, "category.json")}
   end
 
@@ -10,10 +13,14 @@ defmodule IntoTheBookmarks.CategoryView do
   end
 
   def render("category.json", %{category: category}) do
+    # use the Bookmark view to render the list of bookmarks
+    %{ data: bookmarks} = BookmarkView.render("index.json", bookmarks: category.bookmarks)
+
     %{id: category.id,
       category_name: category.category_name,
       category_notes: category.category_notes,
-      user_id: category.user_id
+      user_id: category.user_id,
+      bookmarks: bookmarks
     }
   end
 end
