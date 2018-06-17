8dd5a4484bab5474d5ab05b9f25393aa70b59566
Robley <robleyadrian@gmail.com>
2018-06-17 19:48:52 +0300

    update to fix failing tests.

--- api/test/controllers/category_controller_test.exs
+++ api/test/controllers/category_controller_test.exs
@@ -6,6 +6,8 @@ defmodule IntoTheBookmarks.CategoryControllerTest do
   @invalid_attrs %{}
 
   setup %{conn: conn} do
+    conn = conn
+      |> Plug.Test.init_test_session(current_user_id: 1)
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
