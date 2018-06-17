e5475296d29aad8af1bddcdf9694cebefdb36c53
Robley <robleyadrian@gmail.com>
2018-06-17 19:48:38 +0300

    update to fix failing tests.

--- api/test/controllers/bookmark_controller_test.exs
+++ api/test/controllers/bookmark_controller_test.exs
@@ -2,21 +2,23 @@ defmodule IntoTheBookmarks.BookmarkControllerTest do
   use IntoTheBookmarks.ConnCase
 
   alias IntoTheBookmarks.Bookmark
-  @valid_attrs %{bookmark_notes: "some bookmark_notes", bookmark_title: "some bookmark_title", bookmark_url: "some bookmark_url"}
+  @valid_attrs %{bookmark_notes: "some bookmark_notes", bookmark_title: "some bookmark_title", bookmark_url: "some bookmark_url", user_id: 1, category_id: 1}
   @invalid_attrs %{}
 
   setup %{conn: conn} do
+    conn = conn
+      |> Plug.Test.init_test_session(current_user_id: 1)
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
   test "lists all entries on index", %{conn: conn} do
-    conn = get conn, bookmark_path(conn, :index)
+    conn = get conn, category_path(conn, :index)
     assert json_response(conn, 200)["data"] == []
   end
 
   test "shows chosen resource", %{conn: conn} do
     bookmark = Repo.insert! %Bookmark{}
-    conn = get conn, bookmark_path(conn, :show, bookmark)
+    conn = get conn, category_path(conn, :show, bookmark)
     assert json_response(conn, 200)["data"] == %{"id" => bookmark.id,
       "bookmark_title" => bookmark.bookmark_title,
       "bookmark_url" => bookmark.bookmark_url,
@@ -27,37 +29,37 @@ defmodule IntoTheBookmarks.BookmarkControllerTest do
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
     assert_error_sent 404, fn ->
-      get conn, bookmark_path(conn, :show, -1)
+      get conn, category_path(conn, :show, -1)
     end
   end
 
   test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, bookmark_path(conn, :create), bookmark: @valid_attrs
+    conn = post conn, category_path(conn, :create), bookmark: @valid_attrs
     assert json_response(conn, 201)["data"]["id"]
     assert Repo.get_by(Bookmark, @valid_attrs)
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, bookmark_path(conn, :create), bookmark: @invalid_attrs
+    conn = post conn, category_path(conn, :create), bookmark: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "updates and renders chosen resource when data is valid", %{conn: conn} do
     bookmark = Repo.insert! %Bookmark{}
-    conn = put conn, bookmark_path(conn, :update, bookmark), bookmark: @valid_attrs
+    conn = put conn, category_path(conn, :update, bookmark), bookmark: @valid_attrs
     assert json_response(conn, 200)["data"]["id"]
     assert Repo.get_by(Bookmark, @valid_attrs)
   end
 
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
     bookmark = Repo.insert! %Bookmark{}
-    conn = put conn, bookmark_path(conn, :update, bookmark), bookmark: @invalid_attrs
+    conn = put conn, category_path(conn, :update, bookmark), bookmark: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "deletes chosen resource", %{conn: conn} do
     bookmark = Repo.insert! %Bookmark{}
-    conn = delete conn, bookmark_path(conn, :delete, bookmark)
+    conn = delete conn, category_path(conn, :delete, bookmark)
     assert response(conn, 204)
     refute Repo.get(Bookmark, bookmark.id)
   end
