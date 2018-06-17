169bc7e5188f37d35d7e74b21850b1b474f01057
Robley <robleyadrian@gmail.com>
2018-06-17 23:11:33 +0300

    modify routing to fix tests.

--- api/web/router.ex
+++ api/web/router.ex
@@ -21,6 +21,9 @@ defmodule IntoTheBookmarks.Router do
     resources "/users", UserController, except: [:new, :edit]
     resources "/categories/:id/bookmarks", BookmarkController, except: [:new, :edit]
     resources "/categories", CategoryController, except: [:new, :edit]
+    # resources "/categories", CategoryController do
+    #   resources "/bookmarks", BookmarkController, except: [:new, :edit]
+    # end
   end
 
   # Plug function
