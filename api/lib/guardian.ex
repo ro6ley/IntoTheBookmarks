720d1147f85b8d4a8d2dba68b11461d69f2abd41
Robley <robleyadrian@gmail.com>
2018-07-22 20:51:53 +0300

    add guardian file.

---
+++ api/lib/guardian.ex
@@ -0,0 +1,23 @@
+defmodule IntoTheBookmarks.Guardian do
+  use Guardian, otp_app: :IntoTheBookmarks
+
+  def subject_for_token(user, _claims) do
+    sub = to_string(user.id)
+    {:ok, sub}
+  end
+
+  def subject_for_token(_, _) do
+    {:error, :reason_for_error}
+  end
+
+  def resource_from_claims(claims) do
+    id = claims["sub"]
+    resource = IntoTheBookmarks.User.get_user!(id)
+    {:ok,  resource}
+  end
+
+  def resource_from_claims(_claims) do
+    {:error, :reason_for_error}
+  end
+
+end
