37a514665decc3a1c68e8d55c58cbccb88e118ac
Robley <robleyadrian@gmail.com>
2018-07-30 23:36:18 +0300

    add navbar component

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <nav>
+    <div class="nav-wrapper green">
+      <a href="#!" class="brand-logo">🐼</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="sass.html">Sass</a></li>
+    <li><a href="badges.html">Components</a></li>
+    <li><a href="collapsible.html">Javascript</a></li>
+    <li><a href="mobile.html">Mobile</a></li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  // receive a list of elements and link them
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
