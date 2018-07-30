876e9349d17fa82cb8e7704588904b2dec483518
Robley <robleyadrian@gmail.com>
2018-07-30 23:37:02 +0300

    add new component routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Bookmarks from '@/components/Bookmarks'
+import HomePage from '@/components/HomePage'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
@@ -10,6 +15,31 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/index',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/home',
+      name: 'HomePage',
+      component: HomePage
+    },
+    {
+      path: '/bookmarks',
+      name: 'Bookmarks',
+      component: Bookmarks
     }
   ]
 })
